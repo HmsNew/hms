df6f657c357686d9667e785fc6dad15ec9e6020a
HmsNew <99550583+HmsNew@users.noreply.github.com>
2022-02-13 19:06:07 +0200

    Add files via upload

---
+++ HmsRobot/modules/alive.py
@@ -0,0 +1,24 @@
+import os
+import re
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from HmsRobot.events import register
+from HmsRobot import telethn as tbot
+
+
+PHOTO = "https://telegra.ph/file/991c94db16a466f580686.jpg"
+
+@register(pattern=("/alive"))
+async def awake(event):
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Hms.** \n\n"
+  TEXT += "⚪ **I'm Working Properly** \n\n"
+  TEXT += f"⚪ **My Master : [Queen](https://t.me/hms_01)** \n\n"
+  TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
+  TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += "**Thanks For Adding Me Here ❤️**"
+  BUTTON = [[Button.url("Update", "https://t.me/botatiiii"), Button.url("Support", "https://t.me/dd3mhms")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
